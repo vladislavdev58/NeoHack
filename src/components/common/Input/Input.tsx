043167b0dafc60661351e5c49de85f18ba06aeeb
vladislavdev58 <vladislav.dev58@gmail.com>
2022-02-18 22:28:57 +0300

    Компонет инпута

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,15 +1,30 @@
 import React, {FC} from 'react'
 import {Field} from 'formik'
+import clsx from 'clsx'
+import './Input.scss'
 
 type MyProps = {
     placeholder: string
-    value: string
+    type: 'text' | 'email' | 'password'
+    name: string
+    error?: string
+    disabled?: boolean
+    theme?: 'light' | 'dark'
 }
 
-export const Input: FC<MyProps> = (props) => {
+export const Input: FC<MyProps> = ({
+                                       placeholder,
+                                       type,
+                                       name,
+                                       error,
+                                       disabled = false,
+                                       theme = 'dark'
+                                   }) => {
     return (
-        <div>
-            <Field {...props}/>
+        <div className='input'>
+            <Field placeholder={placeholder} type={type} name={name} disabled={disabled}
+                   className={clsx('input__field', theme === 'light' && 'input__field_light',error && 'input__field_error')}/>
+            {error && <p className="message_error">{error}</p>}
         </div>
     )
 }
