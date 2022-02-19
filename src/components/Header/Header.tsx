c7e3b9448cd9c82c4b26ab928f6e7e06413a598f
vladislavdev58 <vladislav.dev58@gmail.com>
2022-02-19 16:11:38 +0300

    TextLogo component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './Header.scss'
 import {Avatar} from '../common/Avatar/Avatar'
 import {Notification} from '../common/Notification/Notification'
+import {TextLogo} from '../common/TextLogo/TextLogo'
 
 export const Header = () => {
     return (
         <div className='header'>
-            <div className="header__title">RateCoin🚀</div>
+            <TextLogo/>
             <Notification num={9}/>
             <Avatar url='https://cdnimg.rg.ru/i/gallery/e9f32f77/12_4967d989.jpg'/>
         </div>
---
+++ src/components/common/TextLogo/TextLogo.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './TextLogo.scss'
+
+export const TextLogo = () => {
+    return (
+        <div className="text-logo">RateCoin🚀</div>
+    )
+}
