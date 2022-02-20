510cedd7c704f9464b0e7ce9219a235c188725dc
noname.with.username <75997933+vladislavdev58@users.noreply.github.com>
2022-02-20 08:17:59 +0300

    logout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,10 @@ import {Avatar} from '../common/Avatar/Avatar'
 import {Notification} from '../common/Notification/Notification'
 import {TextLogo} from '../common/TextLogo/TextLogo'
 import {Burger} from '../common/Burger/Burger'
+import {Button} from '../common/Button/Button'
+import {useCookies} from 'react-cookie'
+import UserStore from '../../store/UserStore'
+import {runInAction} from 'mobx'
 
 type MyProps = {
     isOpenMenu: boolean
@@ -11,12 +15,20 @@ type MyProps = {
 }
 
 export const Header:FC<MyProps> = ({isOpenMenu, setIsOpenMenu}) => {
+    const [cookies, setCookies, removeCookie] = useCookies()
+    const logout = () => {
+        removeCookie('Auth-Token')
+        runInAction(() => {
+            UserStore.token = null
+        })
+    }
     return (
         <div className='header'>
             <Burger isOpenMenu={isOpenMenu} setIsOpenMenu={setIsOpenMenu}/>
             <TextLogo/>
             <Notification/>
             <Avatar url='https://cdnimg.rg.ru/i/gallery/e9f32f77/12_4967d989.jpg'/>
+            <Button action={logout}>Выход</Button>
         </div>
     )
 }
